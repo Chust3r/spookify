77f80a4f14dc66b9252c9d65a2dd1c1cf797be1b
Chust3r <kriptoortega@gmail.com>
2024-10-13 17:12:54 -0600

    Update description

--- components/description.tsx
+++ components/description.tsx
@@ -1,10 +1,10 @@
-'use client' // Asegúrate de que el componente use renderizado del lado del cliente
+'use client'
 
 import { m } from 'framer-motion'
 
 export function Description() {
 	return (
-		<m.p className='max-w-3xl text-pretty text-muted-foreground/50 text-2xl font-rage'>
+		<m.p className='max-w-3xl text-pretty text-muted-foreground/70 text-2xl font-rage'>
 			Upload, Transform & Share Your Spooky Side
 		</m.p>
 	)
