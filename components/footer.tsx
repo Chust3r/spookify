5894ac7d1c2456147944624d3048c7a522fb2511
Chust3r <kriptoortega@gmail.com>
2024-10-13 16:26:54 -0600

    Add Footer

---
+++ components/footer.tsx
@@ -0,0 +1,19 @@
+import { PumpkinSprite } from './sprites/pumpkin'
+
+export function Footer() {
+	return (
+		<footer className='text-muted-foreground py-3 flex items-center justify-center flex-col gap-2'>
+			<div className='w-full h-10 justify-center items-center flex'>
+				<div className='absolute'>
+					<PumpkinSprite />
+				</div>
+			</div>
+			<div>
+				<p className='text-xs text-muted-foreground font-rage relative'>
+					<span className='absolute w-full h-full bg-neutral-50 blur opacity-10'/>
+					Trick or treat, let the spooky night begin
+				</p>
+			</div>
+		</footer>
+	)
+}
