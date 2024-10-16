55a4473cc27871ef3be2449eb0c09081173ca3d7
Chust3r <kriptoortega@gmail.com>
2024-10-15 18:16:54 -0600

    Render Image

--- components/cloudinary/render-image.tsx
+++ components/cloudinary/render-image.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { getCldImageUrl } from 'next-cloudinary'
 import Image from 'next/image'
+import { useCloudinaryImage } from '~/hooks/useCloudinaryImage'
+import { PumpkinTwoSprite } from '../sprites/pumpkin-two'
 
 interface Props {
 	cloudinaryPublicId: string
@@ -11,14 +13,30 @@ export function RenderImage({ cloudinaryPublicId }: Props) {
 		src: cloudinaryPublicId,
 	})
 
+	const { isLoading, error } = useCloudinaryImage(url)
+
+	if (error) {
+		return <div>{error.message}</div>
+	}
+
+	if (isLoading)
+		return (
+			<div className='w-[400px] h-[300px] bg-neutral-800/20 backdrop-blur rounded-lg relative overflow-hidden grid place-content-center gap-2'>
+				<PumpkinTwoSprite />
+				<span className='text-muted-foreground font-rage'>Loading</span>
+			</div>
+		)
+
 	return (
 		<div className='w-[400px] h-[300px] bg-neutral-800/20 backdrop-blur rounded-lg relative overflow-hidden'>
-			<Image
-				src={url}
-				alt='Original Image'
-				fill
-				className='w-full h-full object-contain rounded overflow-hidden'
-			/>
+			{!isLoading && (
+				<Image
+					src={url}
+					alt='Original Image'
+					fill
+					className='w-full h-full object-contain rounded overflow-hidden'
+				/>
+			)}
 		</div>
 	)
 }
